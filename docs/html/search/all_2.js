64bc77d2179b5fcc56cabbbae0c81797eb0b9b4e
yadev83 <50366613+yadev83@users.noreply.github.com>
2021-01-08 13:43:25 +0100

    Hotfixes HTML doc to enable GitHub pages

---
+++ docs/html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['request_2',['Request',['../classrequest_1_1_request.html',1,'request']]],
+  ['request_2epy_3',['request.py',['../request_8py.html',1,'']]]
+];
---
+++ docs/html/search/files_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['request_2epy_6',['request.py',['../request_8py.html',1,'']]]
+];
